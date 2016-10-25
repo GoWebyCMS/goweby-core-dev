d8f10e67f7ba83117ec8a0cf923382027ba77a29
Katerina Kampardi <kkampardi@gmail.com>
2016-10-25 18:19:11 +0300

    Add basic UT for portfolio app #4
    - Test urlresolvers

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from portfolio.views import home_page
 
 # Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/portfolio')
+        self.assertEqual(found.func, home_page)
