981d90451ac7435e56d7b8c6709362c6d0b386b3
Katerina Kampardi <kkampardi@gmail.com>
2016-10-25 20:23:30 +0300

    Add UT for home_page view #4

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from portfolio.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 
@@ -9,3 +10,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/portfolio')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Portfolio App</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
