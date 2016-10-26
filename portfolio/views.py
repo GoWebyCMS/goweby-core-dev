3ae1ca807ae4534e41d74571afcdb16a1367b5b5
Katerina Kampardi <kkampardi@gmail.com>
2016-10-26 11:36:51 +0300

    Refactor home page view to use a template #5
    - Add the tamplate
    - Add unit tests

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    return HttpResponse('<html><title>Portfolio App</title></html>')
+    return render(request, 'home.html')
