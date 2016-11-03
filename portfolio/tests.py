0c56751266da88ba4a0e39181f0d5de7aed6a3f1
Katerina Kampardi <kkampardi@gmail.com>
2016-11-03 12:04:39 +0200

    Add Model for Portfolio Data #7 - Model for portfolio items - UT for portfolio model - Associated migration

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -4,6 +4,8 @@ from portfolio.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from portfolio.models import Project
+
 # Create your tests here.
 
 class HomePageTest(TestCase):
@@ -16,4 +18,23 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         # test implementation instead of testing constants
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(response.content.decode(), response)
+
+
+class ProjectModelTest(TestCase):
+    def test_saving_retrieving_projects(self):
+        first_project = Project()
+        first_project.name = "The first ever test project"
+        first_project.save()
+
+        second_project = Project()
+        second_project.name = "The second test project"
+        second_project.save()
+
+        saved_projects = Project.objects.all()
+        self.assertEqual(saved_projects.count(),2)
+
+        first_saved_project = saved_projects[0]
+        second_saved_project = saved_projects[1]
+        self.assertEqual(first_saved_project.name,'The first ever test project')
+        self.assertEqual(second_saved_project.name, 'The second test project')
