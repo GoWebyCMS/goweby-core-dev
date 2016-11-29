0c61630185d120d6b7da6e4fed9f8cc22c5521fc
Katerina Kampardi <kkampardi@gmail.com>
2016-11-29 18:40:09 +0200

    Customize admin for porftolio app #11
    - Add basic administration for the portfolio app
    - Update model Project: add slugfield and set it prepopulated

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Project
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    list_display = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Project, ProjectAdmin)
