ccf5dbf001e12e27e8e0d1f1b41a3c768884611b
Katerina Kampardi <kkampardi@gmail.com>
2016-11-30 18:11:07 +0200

    Customize category admin #8

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import Project, Category
 # Register your models here.
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -8,4 +8,9 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_description', 'category', 'status', )
     prepopulated_fields = {'slug': ('name',)}
 
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Category, CategoryAdmin)
