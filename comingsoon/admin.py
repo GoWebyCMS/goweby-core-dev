72efb51d2ba426a80baf472ee025b56f8876b185
Katerina Kampardi <kkampardi@gmail.com>
2016-12-09 11:15:00 +0200

    Add Models for Comingsoon App
    - Add Basic Model and IngoreURL helper model
    - Customize and Register models to admin
    - Include the related database migrations

--- comingsoon/admin.py
+++ comingsoon/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comingsoon, IgnoreURL
+
+# Customize Models for Admin
+
+
+class IgnoredURLInline(admin.TabularInline):
+    model = IgnoreURL
+    extra = 2
+
+
+class ComingsoonAdmin(admin.ModelAdmin):
+    inlines = [IgnoredURLInline]
+    list_display = ['__str__', 'active', 'site', 'start', 'end']
+    #readonly_fields = ('site',)
+
+    #disable actions
+    #actions = None
+
+    # TODO: Disable permitions delete and add
+
+
+
+# Register Models
+
+admin.site.register(Comingsoon, ComingsoonAdmin)
--- comingsoon/models.py
+++ comingsoon/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.sites.models import Site
 
 # Create your models here.
+
+class Comingsoon(models.Model):
+    name = models.CharField(max_length=255)
+    site = models.ForeignKey(Site)
+    active = models.BooleanField('Coming Soon Mode', default=False)
+    # count down fields
+    start = models.DateTimeField(blank=True)
+    end = models.DateTimeField(blank=True)
+
+    class Meta:
+        verbose_name = 'Coming Soon'
+        verbose_name_plural = 'Coming Soon'
+
+    def __str__(self):
+        return self.name
+
+class IgnoreURL(models.Model):
+    comingsoon = models.ForeignKey(Comingsoon)
+    pattern = models.CharField(max_length=200)
+    about = models.CharField(max_length=100, help_text="About this URL")
+
+    def __str__(self):
+        return self.pattern
